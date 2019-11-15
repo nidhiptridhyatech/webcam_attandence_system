49609814fc13c4612495f42046fceb6edffbf14e
Nidhi <nidhiptridhyatech@gmail.com>
2019-11-15 15:27:42 +0530

    initial commit

---
+++ resources/views/vendor/voyager/users/edit-add.blade.php
@@ -0,0 +1,163 @@
+@extends('voyager::master')
+
+@section('page_title', __('voyager::generic.'.(isset($dataTypeContent->id) ? 'edit' : 'add')).' '.$dataType->getTranslatedAttribute('display_name_singular'))
+
+@section('css')
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+@stop
+
+@section('page_header')
+    <h1 class="page-title">
+        <i class="{{ $dataType->icon }}"></i>
+        {{ __('voyager::generic.'.(isset($dataTypeContent->id) ? 'edit' : 'add')).' '.$dataType->getTranslatedAttribute('display_name_singular') }}
+    </h1>
+@stop
+
+@section('content')
+    <div class="page-content container-fluid">
+        <form class="form-edit-add" role="form"
+              action="@if(!is_null($dataTypeContent->getKey())){{ route('voyager.'.$dataType->slug.'.update', $dataTypeContent->getKey()) }}@else{{ route('voyager.'.$dataType->slug.'.store') }}@endif"
+              method="POST" enctype="multipart/form-data" autocomplete="off">
+            <!-- PUT Method if we are editing -->
+            @if(isset($dataTypeContent->id))
+                {{ method_field("PUT") }}
+            @endif
+            {{ csrf_field() }}
+
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="panel panel-bordered">
+                    {{-- <div class="panel"> --}}
+                        @if (count($errors) > 0)
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+
+                        <div class="panel-body">
+                            <div class="form-group">
+                                <label for="name">{{ __('voyager::generic.name') }}</label>
+                                <input type="text" class="form-control" id="name" name="name" placeholder="{{ __('voyager::generic.name') }}"
+                                       value="{{ old('name', $dataTypeContent->name ?? '') }}">
+                            </div>
+                            <div class="form-group">
+                                <label for="name">First Name</label>
+                                <input type="text" class="form-control" id="first_name" name="first_name" placeholder="First Name"
+                                value="{{ old('first_name', $dataTypeContent->first_name ?? '') }}">
+                            </div>
+                            <div class="form-group">
+                                <label for="name">Last Name</label>
+                                <input type="text" class="form-control" id="last_name" name="last_name" placeholder="Last Name"
+                                value="{{ old('last_name', $dataTypeContent->last_name ?? '') }}">
+                            </div>
+                            <div class="form-group">
+                                <label for="default_role">Parent User</label>
+                                <select class="form-control select2-ajax select2-hidden-accessible parent_id" name="parent_id" tabindex="-1" aria-hidden="true">
+                                        <option value="" >None</option>
+                                    @foreach($users as $user)
+                                        <option value="{{$user->id}}"@if($dataTypeContent->parent_id == $user->id)selected @endif>{{$user->name}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="email">{{ __('voyager::generic.email') }}</label>
+                                <input type="email" class="form-control" id="email" name="email" placeholder="{{ __('voyager::generic.email') }}"
+                                       value="{{ old('email', $dataTypeContent->email ?? '') }}">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="password">{{ __('voyager::generic.password') }}</label>
+                                @if(isset($dataTypeContent->password))
+                                    <br>
+                                    <small>{{ __('voyager::profile.password_hint') }}</small>
+                                @endif
+                                <input type="password" class="form-control" id="password" name="password" value="" autocomplete="new-password">
+                            </div>
+                            <div class="form-group">
+                                <label for="name">Phone Number</label>
+                                <input type="text" class="form-control" id="phone_number" name="phone_number" placeholder="Phone Number"
+                                value="{{ old('phone_number', $dataTypeContent->phone_number ?? '') }}">
+                            </div>
+
+                            @can('editRoles', $dataTypeContent)
+                                <div class="form-group">
+                                    <label for="default_role">{{ __('voyager::profile.role_default') }}</label>
+                                    @php
+                                        $dataTypeRows = $dataType->{(isset($dataTypeContent->id) ? 'editRows' : 'addRows' )};
+
+                                        $row     = $dataTypeRows->where('field', 'user_belongsto_role_relationship')->first();
+                                        $options = $row->details;
+                                    @endphp
+                                    @include('voyager::formfields.relationship')
+                                </div>
+                               <!--  <div class="form-group">
+                                    <label for="additional_roles">{{ __('voyager::profile.roles_additional') }}</label>
+                                    @php
+                                        $row     = $dataTypeRows->where('field', 'user_belongstomany_role_relationship')->first();
+                                        $options = $row->details;
+                                    @endphp
+                                    @include('voyager::formfields.relationship')
+                                </div> -->
+                            @endcan
+                            @php
+                            if (isset($dataTypeContent->locale)) {
+                                $selected_locale = $dataTypeContent->locale;
+                            } else {
+                                $selected_locale = config('app.locale', 'en');
+                            }
+
+                            @endphp
+                            <div class="form-group">
+                                <label for="locale">{{ __('voyager::generic.locale') }}</label>
+                                <select class="form-control select2" id="locale" name="locale">
+                                    @foreach (Voyager::getLocales() as $locale)
+                                    <option value="{{ $locale }}"
+                                    {{ ($locale == $selected_locale ? 'selected' : '') }}>{{ $locale }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="panel panel panel-bordered panel-warning">
+                        <div class="panel-body">
+                            <div class="form-group">
+                                @if(isset($dataTypeContent->avatar))
+                                    <img src="{{ filter_var($dataTypeContent->avatar, FILTER_VALIDATE_URL) ? $dataTypeContent->avatar : Voyager::image( $dataTypeContent->avatar ) }}" style="width:200px; height:auto; clear:both; display:block; padding:2px; border:1px solid #ddd; margin-bottom:10px;" />
+                                @endif
+                                <input type="file" data-name="avatar" name="avatar">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <button type="submit" class="btn btn-primary pull-right save">
+                {{ __('voyager::generic.save') }}
+            </button>
+        </form>
+
+        <iframe id="form_target" name="form_target" style="display:none"></iframe>
+        <form id="my_form" action="{{ route('voyager.upload') }}" target="form_target" method="post" enctype="multipart/form-data" style="width:0px;height:0;overflow:hidden">
+            {{ csrf_field() }}
+            <input name="image" id="upload_file" type="file" onchange="$('#my_form').submit();this.value='';">
+            <input type="hidden" name="type_slug" id="type_slug" value="{{ $dataType->slug }}">
+        </form>
+    </div>
+@stop
+
+@section('javascript')
+    <script>
+        $('document').ready(function () {
+            $(".parent_id").select2({});
+            $('.toggleswitch').bootstrapToggle();
+        });
+    </script>
+@stop
