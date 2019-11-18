a36b0f50cd60edc22258350b35ac436ffb1d9537
shreya_shah <shreya.s@tridhya.com>
2019-11-18 14:26:44 +0530

    get faceid and verification process

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
 use Validator;
+use App\FrontUser;
 
 class AttendanceController extends Controller
 {
@@ -12,69 +13,126 @@ class AttendanceController extends Controller
     {
         if(isset($_POST['image']))
         {
-        $validator = Validator::make($request->all(), [
-            'image' => ['required'],
-        ]);
-        if ($validator->fails()) {
-            return redirect()->back()->withErrors(['image' => 'Please provide snapshot to make attendance.']);
-        }
-        
-    print_r($_POST);exit;
-    $img = $_POST['image'];
-    $webcam_path = "images/webcam/users/";
-    $folderPath = public_path($webcam_path);
-    $image_parts = explode(";base64,", $img);
-    $image_type_aux = explode("image/", $image_parts[0]);
-    $image_type = $image_type_aux[1];
-  
-    $image_base64 = base64_decode($image_parts[1]);
-    $fileName = uniqid() . '.png';
-  
-    $file = $folderPath . $fileName;
-    file_put_contents($file, $image_base64);
-    $avatar = $webcam_path.$fileName;
-    $avatar_url = url('/'.$avatar);
-    //test Face API
-    $headers = [
-        'Content-Type' => 'application/json',
-        'Ocp-Apim-Subscription-Key' => 'b3bde8e39f654398bba68bac2a3ae0f5',
-    ];
-    // $body = '{
-    //     "url": "https://content-static.upwork.com/uploads/2014/10/01073427/profilephoto1.jpg"
-    // }';
-    $body = '{
-        "url": "'.$avatar_url.'"
-    }';
-    
-   // $client = new Client();
-    $client = new Client([
-        'headers' => $headers
-    ]);
+            $validator = Validator::make($request->all(), [
+                'image' => ['required'],
+            ]);
+            if ($validator->fails()) {
+                return redirect()->back()->withErrors(['image' => 'Please provide snapshot to make attendance.']);
+            }
+            $frontUsers = FrontUser::where('deleted_at',NULL)->get()->toArray();
+            if(!empty($frontUsers))
+            {
+                $img = $_POST['image'];
+                $webcam_path = "images/webcam/login/";
+                $folderPath = public_path($webcam_path);
+                $image_parts = explode(";base64,", $img);
+                $image_type_aux = explode("image/", $image_parts[0]);
+                $image_type = $image_type_aux[1];
+            
+                $image_base64 = base64_decode($image_parts[1]);
+                $fileName = uniqid() . '.png';
+            
+                $file = $folderPath . $fileName;
+                file_put_contents($file, $image_base64);
+                $avatar = $webcam_path.$fileName;
+                $avatar_url = url('/'.$avatar);
+               foreach($frontUsers as $f=>$fuser)
+               {
+                   if(isset($fuser['avatar']))
+                   {
+                    // $login_face_url = "https://content-static.upwork.com/uploads/2014/10/01073427/profilephoto1.jpg";
+                    // $front_face_url = "https://content-static.upwork.com/uploads/2014/10/01073427/profilephoto1.jpg";     
+                    $login_face_url = $avatar_url;
+                    $front_face_url = url('/storage/'.$fuser['avatar']);
+                    $login_face_id = $this->getFaceIdFromImage($login_face_url);
+                    $front_face_id = $this->getFaceIdFromImage($front_face_url);
+                    $is_verified = $this->verifyFaces($login_face_id,$front_face_id);
+                    if($is_verified == true)
+                    {
+                        //make attendance logic (user_id, lati,long,address,face_url,created_at,updated_at,deleted_at)
+                        // $user = User::create([
+                        //     'name' => $data['name'],
+                        //     'email' => $data['email'],
+                        //     'password' => Hash::make($data['password']),
+                        //     'avatar' => $avatar,
+                        //     'face_id' => $face_id,
+                        // ]);
 
-    $res = $client->request('POST', 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=true', [
-        'body' => $body
-    ]);
-
-    if ($res->getStatusCode() == 200) { // 200 OK
-        $response_data = json_decode($res->getBody()->getContents());
-        $face_id = $response_data[0]->faceId;
-        //$face_id = collect(json_decode($res->getBody()->getContents(),true))->pluck('faceId')->first();
-        //print"<pre>";print_r($response_data[0]->faceId);exit;
+                    }
+                    else{
+                        //remove uploaded image from login attempt 
+                        return redirect()->back()->withErrors(['image' => 'Sorry, Face not verified! Please try again.']);   
+                    }
+                   }
+               } 
+            }
+            }
+            return view("attendance");
     }
-    //Face API end
-        $user = User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-            'avatar' => $avatar,
-            'face_id' => $face_id,
+    public function getFaceIdFromImage($face_url)
+    {
+        $headers = [
+            'Content-Type' => 'application/json',
+            'Ocp-Apim-Subscription-Key' => config('faceapi.subscription_key'),
+        ];
+        $body = '{
+            "url": "'.$face_url.'"
+        }';
+        $client = new Client([
+            'headers' => $headers
         ]);
-        // if (isset($data['avatar'])) {
-        //     $user->addMediaFromRequest('avatar')->toMediaCollection('avatars');
-        // }
+        try{
+            $res = $client->request('POST', config('faceapi.api_endpoint').'detect?returnFaceId=true', [
+                'body' => $body
+            ]);
     
+            if ($res->getStatusCode() == 200) { // 200 OK
+                $response_data = json_decode($res->getBody()->getContents());
+                $face_id = $response_data[0]->faceId;
+            }
         }
-        return view("attendance");
+        catch (\Exception $ex)
+        {
+        return redirect()->back()->withErrors(['image' => 'Sorry, Face not identified! Please try again.']);
+        }
+        return $face_id;
+    }
+    public function verifyFaces($login_face_id,$front_user_face_id)
+    {
+        $headers = [
+            'Content-Type' => 'application/json',
+            'Ocp-Apim-Subscription-Key' => config('faceapi.subscription_key'),
+        ];
+        $body = '{
+            "faceId1": "'.$login_face_id.'",
+            "faceId2": "'.$front_user_face_id.'",
+        }';
+        $client = new Client([
+            'headers' => $headers
+        ]);
+        try
+        {
+            $res = $client->request('POST', config('faceapi.api_endpoint').'verify', [
+                'body' => $body
+            ]);
+            if ($res->getStatusCode() == 200) { // 200 OK
+                $response_data = json_decode($res->getBody()->getContents());
+                if($response_data->isIdentical == 1 && $response_data->confidence >= 0.7)
+                {
+                    $verified = true;
+                }
+                else
+                {
+                    $verified = false;
+                }
+            }
+        }
+        catch (\Exception $ex)
+        {
+        return redirect()->back()->withErrors(['image' => 'Sorry, Face not identified! Please try again.']);
+        }
+
+        return $verified;
     }
     public function getUserLocation(Request $request)
     {
