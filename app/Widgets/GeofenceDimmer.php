c1e14eafeb2219e806c5f8dbc12ebfa520f21e41
Nidhi <nidhiptridhyatech@gmail.com>
2019-11-19 16:48:29 +0530

    changes

---
+++ app/Widgets/GeofenceDimmer.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Widgets;
+
+use TCG\Voyager\Widgets\BaseDimmer;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+use TCG\Voyager\Facades\Voyager;
+
+class GeofenceDimmer extends BaseDimmer
+{
+    /**
+     * The configuration array.
+     *
+     * @var array
+     */
+    protected $config = [];
+
+    /**
+     * Treat this method as a controller action.
+     * Return view() or other content to display.
+     */
+    public function run()
+    {
+        $count = \App\Geofence::count();
+        $string = 'Geofence';
+        $text = 'You have '.$count.' geofence in your database. Click on button below to view all front user.';
+        return view('voyager::dimmer', array_merge($this->config, [
+            'icon'   => 'voyager-group',
+            'title'  => "{$count} {$string}",
+            'text'   => $text,
+            'button' => [
+                'text' => 'View all geofence',
+                'link' => route('voyager.geofences.index'),
+            ],
+            'image' => voyager_asset('images/widget-backgrounds/01.jpg'),
+        ]));
+    }
+
+    /**
+     * Determine if the widget should be displayed.
+     *
+     * @return bool
+     */
+    public function shouldBeDisplayed()
+    {
+        return Auth::user()->can('browse', Voyager::model('User'));
+    }
+}
